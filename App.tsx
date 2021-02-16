8ee7158dd19a5678a435d9b0e9b0a9d4ecb071dc
澤西　義人 <uu182094@CA6467.local>
2021-02-16 13:30:12 +0900

    タイトルの追加

--- App.tsx
+++ App.tsx
@@ -1,12 +1,37 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import { FontAwesome5,MaterialIcons } from '@expo/vector-icons';
 import AddTodo from './src/app/components/AddTodo'
 import ToDoList from './src/app/components/TodoList'
 
 
 const Tab = createBottomTabNavigator();
+
+const AddToDoStack = createStackNavigator()
+const ToDoListStack = createStackNavigator()
+
+const AddTodoStackComponet = () => {
+  return(
+    <AddToDoStack.Navigator>
+      <AddToDoStack.Screen
+        name="タスクの追加"
+        component={AddTodo}
+      />
+    </AddToDoStack.Navigator>
+  )
+}
+const ToDoListStackComponet = () => {
+  return(
+    <ToDoListStack.Navigator>
+      <ToDoListStack.Screen
+        name="タスクの一覧"
+        component={ToDoList}
+      />
+    </ToDoListStack.Navigator>
+  )
+}
 
 const AddTodoIcon: React.FC<{color:string,size:number}> = ({color,size}) => (<MaterialIcons name="add-task" size={size} color={color} />)
 const ToDoListIcon: React.FC<{color:string,size:number}> = ({color,size}) => (<FontAwesome5 name="tasks" size={size} color={color} />)
@@ -16,13 +41,13 @@ export default function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen
-        name="タスクを作成"
-        component={AddTodo}
+        name="AddTodoStack"
+        component={AddTodoStackComponet}
         options={{tabBarIcon:AddTodoIcon}} />
 
         <Tab.Screen
-        name="タスク一覧"
-        component={ToDoList}
+        name="ToDoListStack"
+        component={ToDoListStackComponet}
         options={{tabBarIcon:ToDoListIcon}}  />
       </Tab.Navigator>
     </NavigationContainer>
