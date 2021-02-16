23009f23c159866db506ae02d994748b5ff5a057
澤西　義人 <uu182094@CA6467.local>
2021-02-16 12:13:26 +0900

    アイコン追加

--- App.tsx
+++ App.tsx
@@ -1,18 +1,29 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesome5,MaterialIcons } from '@expo/vector-icons';
 import AddTodo from './src/app/components/AddTodo'
 import ToDoList from './src/app/components/TodoList'
 
 
 const Tab = createBottomTabNavigator();
 
+const AddTodoIcon: React.FC<{color:string,size:number}> = ({color,size}) => (<MaterialIcons name="add-task" size={size} color={color} />)
+const ToDoListIcon: React.FC<{color:string,size:number}> = ({color,size}) => (<FontAwesome5 name="tasks" size={size} color={color} />)
+
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="タスクを作成" component={AddTodo} />
-        <Tab.Screen name="タスク一覧" component={ToDoList} />
+        <Tab.Screen
+        name="タスクを作成"
+        component={AddTodo}
+        options={{tabBarIcon:AddTodoIcon}} />
+
+        <Tab.Screen
+        name="タスク一覧"
+        component={ToDoList}
+        options={{tabBarIcon:ToDoListIcon}}  />
       </Tab.Navigator>
     </NavigationContainer>
   );
